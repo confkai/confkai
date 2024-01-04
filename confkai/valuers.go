7336beacb1fba1b0cfb312f54e33f2b4a054b161
Nolan sherman <nolanrsherman@gmail.com>
2024-01-03 20:12:09 -0600

    added test cases for valuers

--- confkai/valuers.go
+++ confkai/valuers.go
@@ -3,6 +3,8 @@ package confkai
 import (
 	"errors"
 	"fmt"
+	"reflect"
+	"runtime"
 	"sync"
 )
 
@@ -37,7 +39,8 @@ func Value(v any) Valuer {
 }
 
 // FirstOf returns the first value from the list
-// whose Valuer does not return an error
+// whose Valuer does not return an error. Returns an
+// error itself if no valid value is found.
 func FirstOf(vals ...Valuer) Valuer {
 	return ValuerFunc(func() (any, error) {
 		errs := make([]error, 0)
@@ -60,7 +63,8 @@ var cachedMap = sync.Map{}
 // be stored and returned for any subsequent calls.
 func Cached(value Valuer) Valuer {
 	return ValuerFunc(func() (any, error) {
-		val, ok := cachedMap.Load(value)
+		hashable := _getFunctionName(value)
+		val, ok := cachedMap.Load(hashable)
 		if ok {
 			// Cache hit
 			return val, nil
@@ -70,7 +74,22 @@ func Cached(value Valuer) Valuer {
 		if err != nil {
 			return nil, err
 		}
-		cachedMap.Store(value, val)
+		cachedMap.Store(hashable, val)
 		return val, nil
+	})
+}
+
+func _getFunctionName(temp interface{}) string {
+	strs := (runtime.FuncForPC(reflect.ValueOf(temp).Pointer()).Name())
+	return strs
+}
+
+// Valuers are lazy loading by default. Eager calls Value() on
+// the passed Valuer immediately and wraps the result as another
+// valuer, effectively "Eager Loading" the  passed value.
+func Eager(value Valuer) Valuer {
+	val, err := value.Value()
+	return ValuerFunc(func() (any, error) {
+		return val, err
 	})
 }
---
+++ confkai/valuers_test.go
@@ -0,0 +1,140 @@
+package confkai_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/confkai/confkai/confkai"
+)
+
+func TestValuerFunc(t *testing.T) {
+
+	result, err := confkai.ValuerFunc(func() (any, error) {
+		return "test", nil
+	}).Value()
+	if result != "test" {
+		t.Error("result should equal test")
+	}
+	if err != nil {
+		t.Error("err should be nil")
+	}
+
+	_, err = confkai.ValuerFunc(func() (any, error) {
+		return "", fmt.Errorf("an error occurred")
+	}).Value()
+	if err == nil {
+		t.Error("err should be not be nil")
+	}
+
+}
+
+func TestValue(t *testing.T) {
+	result, err := confkai.Value("hello world").Value()
+	if err != nil {
+		t.Errorf("err should be nil: %s", err)
+	}
+	if result != "hello world" {
+		t.Error("result should be 'hello world'")
+	}
+}
+
+func TestFirstOf(t *testing.T) {
+	// When the first element errors
+	result, err := confkai.FirstOf(
+		confkai.ValuerFunc(func() (any, error) {
+			return "", fmt.Errorf("test value error")
+		}),
+		confkai.Value("hello world"),
+	).Value()
+	if err != nil {
+		t.Errorf("err should be nil: %s", err)
+	}
+	if result != "hello world" {
+		t.Error("result should be 'hello world'")
+	}
+
+	// when the second element errors
+	result, err = confkai.FirstOf(
+		confkai.Value("hello world"),
+		confkai.ValuerFunc(func() (any, error) {
+			return "", fmt.Errorf("test value error")
+		}),
+	).Value()
+	if err != nil {
+		t.Errorf("err should be nil: %s", err)
+	}
+	if result != "hello world" {
+		t.Error("result should be 'hello world'")
+	}
+
+	// when all elements error
+	_, err = confkai.FirstOf(
+		confkai.ValuerFunc(func() (any, error) {
+			return "", fmt.Errorf("test value error")
+		}),
+		confkai.ValuerFunc(func() (any, error) {
+			return "", fmt.Errorf("test value error")
+		}),
+	).Value()
+	if err == nil {
+		t.Errorf("err should not be nil")
+	}
+
+}
+
+func TestCached(t *testing.T) {
+	delay := time.Duration(3 * time.Second)
+	aLongRunningValue := confkai.ValuerFunc(func() (any, error) {
+		time.Sleep(delay)
+		return "hello world", nil
+	})
+
+	start := time.Now()
+	result, err := confkai.Cached(aLongRunningValue).Value()
+	if err != nil {
+		t.Errorf("err should be nil: %s", err)
+	}
+	if result != "hello world" {
+		t.Error("result should be 'hello world'")
+	}
+	if time.Since(start) < (delay) {
+		t.Error("Should have taken more than 5 seconds")
+	}
+	// test running again, it should be cached now
+	start = time.Now()
+	result, err = confkai.Cached(aLongRunningValue).Value()
+	if err != nil {
+		t.Errorf("err should be nil: %s", err)
+	}
+	if result != "hello world" {
+		t.Error("result should be 'hello world'")
+	}
+	if time.Since(start) > (delay) {
+		t.Error("Should have taken less than 5 seconds")
+	}
+}
+
+func TestEager(t *testing.T) {
+
+	delay := time.Duration(3 * time.Second)
+	aLongRunningValuer := confkai.ValuerFunc(func() (any, error) {
+		time.Sleep(delay)
+		return "hello world", nil
+	})
+
+	start := time.Now()
+	// since this is eager loading it should basically block for the
+	// time that valuer takes to evaluate.
+	myEagerConf := confkai.Eager(aLongRunningValuer)
+	if time.Since(start) < delay {
+		t.Error("Eager did not block as expected")
+	}
+	result, err := myEagerConf.Value()
+	if err != nil {
+		t.Errorf("err should be nil: %s", err)
+	}
+	if result != "hello world" {
+		t.Error("result should be 'hello world'")
+	}
+}
