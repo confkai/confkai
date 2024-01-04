303dfad2b13473ad502d8849ca4496f6ffea30a4
Nolan sherman <nolanrsherman@gmail.com>
2024-01-03 20:11:44 -0600

    added comments

--- confkai/conf.go
+++ confkai/conf.go
@@ -6,22 +6,38 @@ import (
 	"reflect"
 )
 
+// Valuer is the base interface for any configuration type.
+// With this base interface, we can wrap and compose different
+// kinds of values.
 type Valuer interface {
 	Value() (any, error)
 }
 
+// ValuerFunc is a function that implements
+// Valuer. You can use this to create your own
+// Valuers. For Example:
+//
+//	confkai.ValuerFunc(func() (any, error) {
+//		result := os.GetEnv("foo_bar")
+//		return result, nil
+//	})
 type ValuerFunc func() (any, error)
 
+// Value() is ValueFunc's implementation of Valuer
 func (f ValuerFunc) Value() (any, error) {
 	return f()
 }
 
+// Value returns a Valuer that always returns the
+// given value.
 func Value(v any) Valuer {
 	return ValuerFunc(func() (any, error) {
 		return v, nil
 	})
 }
 
+// FirstOf returns the first value from the list
+// whose Valuer does not return an error
 func FirstOf(vals ...Valuer) Valuer {
 	return ValuerFunc(func() (any, error) {
 		errs := make([]error, 0)
@@ -36,6 +52,9 @@ func FirstOf(vals ...Valuer) Valuer {
 	})
 }
 
+// TypeOf asserts that the returned value is of the given type
+// and panics if either the valuer returns an error, or if the
+// type can not be asserted
 func TypeOf[T any](v Valuer) T {
 	val, err := v.Value()
 	if err != nil {
